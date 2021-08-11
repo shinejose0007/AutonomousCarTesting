c259f9c01df0a48e684496acd3d701e040ba1993
shinejose0007 <shinejose0007@users.noreply.github.com>
2021-08-11 06:47:11 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from sumoSimulation.cfg import simulationCfg
-from sumoSimulation.simulation import Simulator
-
-path = "./cfg/OutputGenerated.json"
-cfg = simulationCfg(path)
-simu = Simulator(cfg)
-simu.run()
